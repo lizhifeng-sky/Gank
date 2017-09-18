2fbe0a56555e883a66ac7b3bf7a052244dec7aff
lizhifeng-sky <1740512001@qq.com>
2017-09-18 15:55:15 +0800

    test

--- app/src/main/java/lzf/gank/MainActivity.java
+++ app/src/main/java/lzf/gank/MainActivity.java
@@ -1,6 +1,9 @@
 package lzf.gank;
 
 import lzf.common.base.BaseActivity;
+import lzf.common.network.CustomSubscriber;
+import lzf.common.network.RetrofitProxy;
+import lzf.common.network.bean.UserBean;
 
 public class MainActivity extends BaseActivity {
 
@@ -16,6 +19,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void loadData() {
+        //test
+        RetrofitProxy.getData(apiService.doLogin("", ""), new CustomSubscriber<UserBean>() {
+            @Override
+            public void onSuccess(UserBean userBean) {
 
+            }
+        });
     }
 }
